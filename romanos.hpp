71a6d76053aba9542e48c240923f36b8403bf541
F2419030 Diego do Sacramento <d.sacramento@bb.com.br>
2021-04-03 18:09:04 -0300

    RomanToInt

--- romanos.hpp
+++ romanos.hpp
@@ -2,7 +2,28 @@
 #include <stdlib.h>
 #include <iostream>
 
+//Valores de Roman para Inteiro
 static int RomanToInt(char c) {
+  //Escolha para conversao
+  switch (c)
+  {
+    case 'I':
+      return 1;
+    case 'V':
+      return 5;
+    case 'X':
+      return 10;
+    case 'L':
+      return 50;
+    case 'C':
+      return 100;
+    case 'D':
+      return 500;
+    case 'M':
+      return 1000;
+    default:
+      return 0;
+  }
   return -1;
 }
 
